59f76df87ac6e77620a27bdf99ac100cb7e59119
Innocent-Bern <innocentbern22@gmail.com>
2024-04-08 20:24:31 +0300

    newline

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -11,3 +11,4 @@ export default function createReportObject(employeesList) {
 		}
 	}
 }
+
--- 0x00-ES6_basic/9-main.js
+++ 0x00-ES6_basic/9-main.js
@@ -4,3 +4,4 @@ const fullBudget = getFullBudgetObject(20, 50, 10);
 
 console.log(fullBudget.getIncomeInDollars(fullBudget.income));
 console.log(fullBudget.getIncomeInEuros(fullBudget.income));
+
