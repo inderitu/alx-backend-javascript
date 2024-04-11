a05a1af7e2ea2258f5c0f3452b48b2aa04126648
Innocent-Bern <innocentbern22@gmail.com>
2024-04-11 16:16:00 +0300

    redo task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default async function handleProfileSignup() {
   return createUser()
     .then(async ({ firstName, lastName }) => uploadPhoto()
       .then(({ body }) => {
-        console.log(body, firstName, lastName);
+        console.log(`${body} ${firstName} ${lastName}`);
       }))
     .catch(() => console.log('Signup system offline'));
 }
