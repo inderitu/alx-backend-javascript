cefb23cb12bcbf177e83a454dde03b0b92b9c743
Innocent-Bern <innocentbern22@gmail.com>
2024-04-11 11:21:22 +0300

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from "./utils.js";
 
 export default async function handleProfileSignup() {
-	await createUser()
-		.then(async ({ firstName, lastName }) => {
-			return await uploadPhoto()
-				.then(({ body }) => {
-					console.log(body, firstName, lastName)
-				})
-		})
-		.catch(() => console.log("Signup system offline"))
+  await createUser()
+    .then(async ({ firstName, lastName }) => {
+      return await uploadPhoto()
+        .then(({ body }) => {
+          console.log(body, firstName, lastName)
+        })
+    })
+    .catch(() => console.log("Signup system offline"))
 }
