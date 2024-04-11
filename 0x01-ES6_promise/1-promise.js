ea9ebaacca126312c4b9d387f0679661fe1241ed
Innocent-Bern <innocentbern22@gmail.com>
2024-04-11 14:32:49 +0300

    fixed lint errors

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
 export default async function getFullResponseFromAPI(success) {
-	if (success) {
-		return {
-			status: 200,
-			body: 'Success'
-		}
-	} else {
-		throw new Error("The fake API is not working currently")
-	}
+  if (success) {
+    return {
+      status: 200,
+      body: 'Success',
+    };
+  }
+  throw new Error('The fake API is not working currently');
 }
