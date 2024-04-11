5ff2a802a8c039e2955dad8071c3d88afecbe100
Innocent-Bern <innocentbern22@gmail.com>
2024-04-11 16:37:26 +0300

    redo task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  return createUser()
+  await createUser()
     .then(async ({ firstName, lastName }) => uploadPhoto()
       .then(({ body }) => {
         console.log(`${body} ${firstName} ${lastName}`);
