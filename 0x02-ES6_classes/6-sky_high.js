d76297b2c6040eb1d8b371463f0fd85c74d54d8f
Innocent-Bern <innocentbern22@gmail.com>
2024-04-12 20:25:18 +0300

    task-6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() { return this._floors; }
+
+  set floors(floors) {
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
