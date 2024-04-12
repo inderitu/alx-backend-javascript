829438e05d63d2e551325bbf0b042a38ba65ba20
Innocent-Bern <innocentbern22@gmail.com>
2024-04-12 22:42:59 +0300

    task 9 redo

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,11 @@
+/*
+Implement a class named Car:
+
+Constructor attributes:
+brand (String)
+motor (String)
+color (String)
+Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+Add a method named cloneCar. This method should return a new object of the class.
+Hint: Symbols in ES6
+*/
