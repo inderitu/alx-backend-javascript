badc1e4b3184215b2a5317be95762404054595a5
Innocent-Bern <innocentbern22@gmail.com>
2024-04-16 10:49:28 +0300

    task-0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -9,3 +9,13 @@ Guillaume, id: 1, in San Francisco
 James, id: 2, in Columbia
 Serena, id: 5, in San Francisco
 */
+/**
+ * returns an array of objects
+*/
+export default function getListStudents() {
+  const studentOne = { id: 1, firstName: 'Guillame', loaction: 'San Francisco' };
+  const studentTwo = { id: 2, firstName: 'James', loaction: 'Columbia' };
+  const studentThree = { id: 5, firstName: 'Serena', loaction: 'San Francisco' };
+
+  return [studentOne, studentTwo, studentThree];
+}
