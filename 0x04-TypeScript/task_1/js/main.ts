94ae3a5cfa3d95c8b60a0786d275798e152451c5
Innocent-Bern <innocentbern22@gmail.com>
2024-04-18 02:54:00 +0300

    task 1 done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,11 @@
-/*
-Create a directory task_1 and copy these configuration files into this folder: package.json, tsconfig.json, webpack.config.js
-
-firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
-fullTimeEmployee(boolean) this attribute should always be defined
-yearsOfExperience(number) this attribute is optional
-location(string) this attribute should always be defined
-Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
-Example:
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
 
 const teacher3: Teacher = {
   firstName: 'John',
@@ -17,12 +16,3 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
-
-// should print
-// Object
-// contract: false
-// firstName: "John"
-// fullTimeEmployee: false
-// lastName: "Doe"
-// location: "London"
-*/
