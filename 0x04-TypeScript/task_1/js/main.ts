e0bb6718a14783a458d9e7dacf3e3490326c85d0
Innocent-Bern <innocentbern22@gmail.com>
2024-04-18 03:24:17 +0300

    task 2 done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,32 @@
+/*
+firstName(string) and lastName(string).
+These two attributes should only be modifiable when a Teacher is first initialized
+
+fullTimeEmployee(boolean) this attribute should always be defined
+yearsOfExperience(number) this attribute is optional
+location(string) this attribute should always be defined
+Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
+Example:
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+// should print
+// Object
+// contract: false
+// firstName: "John"
+// fullTimeEmployee: false
+// lastName: "Doe"
+// location: "London"
+*/
+
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -16,3 +45,28 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+/*
+Write an interface named Directors that extends Teacher. It requires an attribute named numberOfReports(number)
+
+Example:
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// should print
+// Object
+// firstName: "John"
+// fullTimeEmployee: true
+// lastName: "Doe"
+// location: "London"
+// numberOfReports: 17
+*/
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
