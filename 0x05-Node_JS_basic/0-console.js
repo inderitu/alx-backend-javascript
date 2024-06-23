2e59f7d81623d3889250e5310348ec524d8e5184
Innocent-Bern <innocentbern22@gmail.com>
2024-06-23 22:08:23 +0300

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * create a function named displayMessage that
+ * prints in STDOUT the string argument.
+*/
+
+function displayMessage(strArg) {
+    console.log(strArg);
+}
+
+module.exports = displayMessage;
